d314aa9e4de703b2408e969a8355325676c503fa
Rodney Cullen <rodcul@gmail.com>
2015-04-02 22:44:28 -0500

    Study Drills

--- ex3.rb
+++ ex3.rb
@@ -1,11 +1,12 @@
+# Outputs text to console
 puts "I will now count my chickens:"
 
 puts "Hens #{25 + 30 / 6}"
-puts "Roosters #{100 - 25 * 3 % 4}"
+puts "Roosters #{100 - 25 * 3.0 % 4}"
 
 puts "Now I will count the eggs:"
 
-puts 3 + 2 + 1 - 5 +4 % 2 -1 /4 +6
+puts 3 + 2 + 1 - 5 +4 % 2.0 -1 /4.0 +6
 
 puts "Is it true that 3 + 2 < 5 -7?"
 
